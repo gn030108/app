e8c3d251dd6b5a62771b51b802c839e922518926
gn030108 <111555871+gn030108@users.noreply.github.com>
2022-08-19 14:38:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ app.config['JSON_AS_ASCII'] = False
 def welcome():
     return render_template('index.html')
 
-@app.route('/login',methods = ['PUT'])
+@app.route('/login',methods = ['GET'])
 def login():
     return render_template("login.html")
 
-@app.route('/put', methods=['PUT'])
+@app.route('/get', methods=['GET'])
 def get():
     f = open('SENSOR.json',"r")
     rf_json = f.read()
